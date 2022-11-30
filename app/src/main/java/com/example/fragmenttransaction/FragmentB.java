77c346e1d5b13b606b95498d22b14b8432e5aa3f
merajuni7ed <merajmusavi1243m@gmail.com>
2022-11-30 23:34:01 +0330

    child fragment

--- app/src/main/java/com/example/fragmenttransaction/FragmentB.java
+++ app/src/main/java/com/example/fragmenttransaction/FragmentB.java
@@ -8,11 +8,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class FragmentB extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_b,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.frame_add,new FragmentChild());
+        fragmentTransaction.commit();
     }
 }
